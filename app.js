502b495c95a15311880067278ca74431f601fbae
Andrew Souza <drewmargielaa@gmail.com>
2020-09-10 20:12:18 -0400

    added random button logic

--- app.js
+++ app.js
@@ -83,3 +83,9 @@ prevBtn.addEventListener("click", function(){
     }
     showPerson(currentItem);
 });
+
+//random person
+randomBtn.addEventListener("click", function(){
+    currentItem = Math.floor(Math.random() * reviews.length);
+    showPerson();
+});
