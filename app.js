ad8c241c8fc9fdee00df6d5df5389de51e54ebc5
Andrew Souza <drewmargielaa@gmail.com>
2020-09-10 20:00:37 -0400

    added next btn function logic

--- app.js
+++ app.js
@@ -65,3 +65,9 @@ const reviews = [
     job.textContent = item.job;
     info.textContent = item.text;
 }
+
+//show next person
+nextBtn.addEventListener("click", function(){
+    currentItem++;
+    showPerson(currentItem);
+});
